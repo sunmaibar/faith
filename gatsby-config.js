4499d8637f4ac0926479797c3280304b9f94a101
Jimmy <jajalaba@yahoo.com.tw>
2020-05-21 15:29:01 +0800

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `國際賢妻良母`,
+    description: `國際賢妻良母主賣德國原裝進口商品
+絕無山寨仿冒品，只賣在台現貨
+提前售完才需預購。
+✔每週美國outlet連線，Michael Kors,Coach,Tory burch等精品,
+空運期(含清關)約2~3週，如有特殊急件再私詢團主訂購。
+✔不定期挖掘日,韓,歐美,澳洲等世界各國商品(連摩洛哥也有過咧～！)
+強烈建議將本社團設計優先通知，
+避免沒搶到好物，槌心肝脾肺就不好了。.`,
+    author: `@Faith`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +31,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cutegirlicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
